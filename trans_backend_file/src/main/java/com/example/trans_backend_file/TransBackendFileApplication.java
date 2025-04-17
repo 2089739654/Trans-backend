8bab628181057662bd1fcc44664afb6521bfcbe5
qwer <2089739654@qq.com>
2025-04-17 21:42:06 +0800

    初始化file模块

---
+++ trans_backend_file/src/main/java/com/example/trans_backend_file/TransBackendFileApplication.java
@@ -0,0 +1,13 @@
+package com.example.trans_backend_file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransBackendFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransBackendFileApplication.class, args);
+    }
+
+}
---
+++ trans_backend_file/src/test/java/com/example/trans_backend_file/TransBackendFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trans_backend_file;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransBackendFileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
