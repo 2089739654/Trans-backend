e2e8e295f44029473e9169f67a8c0349cf44b217
qwer <2089739654@qq.com>
2025-04-17 22:00:46 +0800

    无

---
+++ trans_backend_common/src/test/java/com/example/trans_backend_common/TransBackendCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trans_backend_common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransBackendCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
