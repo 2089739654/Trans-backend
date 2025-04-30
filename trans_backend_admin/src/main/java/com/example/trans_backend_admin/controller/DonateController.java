205947478f2bfe83404c772f52486e5c9e368d35
Nihiler <3529201995@qq.com>
2025-04-30 21:16:14 +0800

    修改了获取微信收款码的方法

--- trans_backend_admin/src/main/java/com/example/trans_backend_admin/controller/DonateController.java
+++ trans_backend_admin/src/main/java/com/example/trans_backend_admin/controller/DonateController.java
@@ -3,6 +3,7 @@ package com.example.trans_backend_admin.controller;
 import com.example.trans_backend_common.common.BaseResponse;
 import com.example.trans_backend_common.common.ResultUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,8 @@ public class DonateController {
     @GetMapping("/donate")
     public BaseResponse<String> donate() throws IOException {
 //        File file = new File("H:\\PROject\\Trans-backend\\trans_backend_admin\\src\\main\\resources\\Pictures\\donate.jpg");
-        byte[] bytes = Files.readAllBytes(Paths.get("H:\\PROject\\Trans-backend\\trans_backend_admin\\src\\main\\resources\\Pictures\\donate.jpg"));
+        InputStream resourceAsStream = DonateController.class.getClassLoader().getResourceAsStream("Pictures/donate.jpg");
+        byte[] bytes = StreamUtils.copyToByteArray(resourceAsStream);
         String s = Base64.getEncoder().encodeToString(bytes);
         return ResultUtils.success(s);
     }
