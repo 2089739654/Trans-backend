79d125664bedff53d1b8ca5f92dd419c39de3401
qwer <2089739654@qq.com>
2025-06-12 13:32:08 +0800

    初始化

---
+++ trans_backend_socket/src/main/java/com/example/trans_backend_socket/SocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.trans_backend_socket;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketApplication {
+
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(SocketApplication.class, args);
+    }
+
+}
