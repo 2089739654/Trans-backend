40d63c1d224b502812cf4a5d8abad20e68368d37
qwer <2089739654@qq.com>
2025-06-13 15:42:21 +0800

    websocket初步

--- trans_backend_socket/src/main/java/com/example/trans_backend_socket/socket/WebSocketHandler.java
+++ trans_backend_socket/src/main/java/com/example/trans_backend_socket/socket/WebSocketHandler.java
@@ -1,18 +1,29 @@
 package com.example.trans_backend_socket.socket;
 
+import cn.hutool.json.JSONUtil;
+import com.example.trans_backend_common.entity.User;
+import com.example.trans_backend_socket.processor.ProcessorChain;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.*;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import javax.annotation.Resource;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+
 @Component
 public class WebSocketHandler extends TextWebSocketHandler {
 
+    @Resource
+    private ProcessorChain processorChain;
+
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-
-
-        System.out.println("WebSocket connection established: " + session.getId());
-        System.out.println(Thread.currentThread().getName());
+        // 在连接建立后执行的逻辑
+        System.out.println("连接建立");
+        processorChain.process(session);
+        // 发送用户加入的消息
     }
 
     @Override
@@ -25,7 +36,15 @@ public class WebSocketHandler extends TextWebSocketHandler {
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        System.out.println("连接结束");
-        System.out.println(Thread.currentThread().getName());
+        // 在连接关闭后执行的逻辑
+
+        // 发送用户离开的消息
+    }
+
+
+    public Long getGroupId(WebSocketSession session) {
+        String path = Objects.requireNonNull(session.getUri()).getPath();
+        Long groupId = Long.valueOf(path.substring(path.lastIndexOf("/") + 1 + "groupId=".length()));
+        return groupId;
     }
 }
